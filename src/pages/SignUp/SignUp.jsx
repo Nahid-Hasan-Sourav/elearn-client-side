e8a90356bd265256dd3df50a787dd8942dc922e4
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2023-08-12 22:58:07 +0600

    added user signUp func

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+
 
 const SignUp = () => {
+  const handleSignIn =(e)=>{
+    e.preventDefault();
+    const form = e.target;
+
+    const name = form.username.value;
+    const email= form.useremail.value;
+    const userImage= form.userImage.files[0];
+    const password = form.password.value;
+
+    const userSignUpInfo={
+      name,
+      email,
+      password,
+      image:userImage,
+    }
+
+    console.log(userSignUpInfo);
+  }
     return (
         <div>
         <div className="flex items-center justify-center w-screen h-screen">
         <div className="w-full max-w-md mx-auto">
-        <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
+        <form onSubmit={handleSignIn} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
             <div>
                 <h2 className="text-2xl font-bold text-center mb-[10px]">SIGNUP</h2>
             </div>
@@ -39,6 +57,7 @@ const SignUp = () => {
               className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               name="userImage"
               type="file"
+              accept="image/*"
             />
           </div>
           <div className="mb-6">
@@ -48,6 +67,7 @@ const SignUp = () => {
             <input
               className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               id="password"
+              name="password"
               type="password"
               placeholder="******************"
             />
@@ -55,7 +75,7 @@ const SignUp = () => {
           <div className="flex items-center justify-between">
             <button
               className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign Up
             </button>
