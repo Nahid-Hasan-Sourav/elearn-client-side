b2057111bd9ffc487f107e0e2e401ca7d7c293cd
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2023-08-14 23:23:16 +0600

    added user,admin,instructor toggle signUp components

---
+++ src/pages/SignUp/Admin/AdminSignUp.jsx
@@ -0,0 +1,87 @@
+
+const AdminSignUp = () => {
+    const handleSignIn =(e)=>{
+        e.preventDefault();
+        const form = e.target;
+    
+        const name = form.username.value;
+        const email= form.useremail.value;
+        const userImage= form.userImage.files[0];
+        const password = form.password.value;
+    
+        const userSignUpInfo={
+          name,
+          email,
+          password,
+          image:userImage,
+        }
+    
+        console.log(userSignUpInfo);
+      }
+    return (
+        <>
+           
+        <form onSubmit={handleSignIn} className="px-8 pt-6 pb-8 mb-4 ">
+           
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
+             Name
+            </label>
+            <input
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              name="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
+             Email
+            </label>
+            <input
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              name="useremail"
+              type="email"
+              placeholder="Useremail"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
+             Image
+            </label>
+            <input
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              name="userImage"
+              type="file"
+              accept="image/*"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
+              Password
+            </label>
+            <input
+              className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+            
+          </div>
+        </form>
+      
+     
+        </>
+    );
+};
+
+export default AdminSignUp;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,92 +1,61 @@
-
+import { useState } from "react";
+import UserSignUp from "./User/UserSignUp";
+import InstructorSignUp from "./Instructor/InstructorSignUp";
+import AdminSignUp from "./Admin/AdminSignUp";
 
 const SignUp = () => {
-  const handleSignIn =(e)=>{
-    e.preventDefault();
-    const form = e.target;
+  const [userType, setUserType] = useState("user");
 
-    const name = form.username.value;
-    const email= form.useremail.value;
-    const userImage= form.userImage.files[0];
-    const password = form.password.value;
+  return (
+    <div className="flex items-center justify-center w-screen h-screen ">
+      <div className="w-full max-w-md mx-auto bg-white rounded shadow-lg">
+        <h1 className="font-bold text-2xl uppercase text-center my-8">
+          Sign Up As
+        </h1>
+        <div className="flex mb-8">
+          <button
+            className={`flex-1 font-bold py-4 text-black ${
+              userType === "admin" ? "bg-blue-500 text-white" : "bg-gray-100"
+            }`}
+            onClick={() => setUserType("admin")}
+          >
+            ADMIN
+          </button>
 
-    const userSignUpInfo={
-      name,
-      email,
-      password,
-      image:userImage,
-    }
+          <button
+            className={`flex-1 font-bold py-4 text-black ${
+              userType === "instructor"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-100"
+            }`}
+            onClick={() => setUserType("instructor")}
+          >
+            INSTRUCTOR
+          </button>
 
-    console.log(userSignUpInfo);
-  }
-    return (
-        <div>
-        <div className="flex items-center justify-center w-screen h-screen">
-        <div className="w-full max-w-md mx-auto">
-        <form onSubmit={handleSignIn} className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
-            <div>
-                <h2 className="text-2xl font-bold text-center mb-[10px]">SIGNUP</h2>
-            </div>
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-             Name
-            </label>
-            <input
-              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              name="username"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-             Email
-            </label>
-            <input
-              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              name="useremail"
-              type="email"
-              placeholder="Useremail"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-             Image
-            </label>
-            <input
-              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              name="userImage"
-              type="file"
-              accept="image/*"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
-              Password
-            </label>
-            <input
-              className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              id="password"
-              name="password"
-              type="password"
-              placeholder="******************"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign Up
-            </button>
-            
-          </div>
-        </form>
-      
-      </div>
-     </div>
+          <button
+            className={`flex-1 font-bold py-4 text-black ${
+              userType === "user" ? "bg-blue-500 text-white" : "bg-gray-100"
+            }`}
+            onClick={() => setUserType("user")}
+          >
+            USER
+          </button>
         </div>
-    );
+        <div
+          className={`transition-opacity duration-1000 ease-in-out mt-4 ${
+            userType === "admin" || userType === "user" || userType === "instructor"
+              ? "opacity-100"
+              : "opacity-0"
+          }`}
+        >
+          {userType === "user" && <UserSignUp />}
+          {userType === "instructor" && <InstructorSignUp />}
+          {userType === "admin" && <AdminSignUp />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
